24ba2689c3e6f1976714121d757f337416bf796d
OUSSAMA MOUNIB <115143701+Itsouss@users.noreply.github.com>
2023-06-17 00:33:23 +0200

    Update python.py

--- python.py
+++ python.py
@@ -1,9 +1,7 @@
 import pandas as pd  #for data manipulation
 import numpy as np #for mathematical calculations
-import seaborn as sns #for visualization
-import pydeck as pdk #for map visualization
+
 import streamlit as st #for BI
-import matplotlib.pyplot as plt
 
 
 ## Read the dataset from the url link in DataGouv
